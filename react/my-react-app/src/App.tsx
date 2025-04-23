b01565c96666b8733d93c53ccef15ea0e55ad0bb
Razif Fadhil Akbar <raziffadhila@gmail.com>
2025-04-23 09:32:17 +0700

    pertemuan 5

---
+++ react/my-react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Hello World</h1>
+     <h2>This is as My app Vite</h2>
+    </>
+  )
+}
+
+export default App
---
+++ react/my-react-app/src/HelloWorld.tsx
@@ -0,0 +1,11 @@
+
+function HelloWorld() {
+    return (
+        <>
+            <h1>Hello Folks!</h1>
+            <h2>Welcome to My App</h2>
+        </>
+    )
+}
+
+export default HelloWorld
